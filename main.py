eef26a28735b7a315fc7614bfc15b2a1be400416
“cw” <wheat940104@gmail.com>
2024-10-24 16:10:34 +0800

    “edit_main.py”

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
 
 @app.get("/")
 def read_root():
@@ -11,3 +16,8 @@ def read_root():
 @app.get("/hello")
 def read_item():
     return "Hello World"
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received", "item": item}
